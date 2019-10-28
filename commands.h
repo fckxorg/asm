ca8038535ccd8669c46d5d770b2205223b0ed578
Maxim Kokryashkin <max.kokryashkin@gmail.com>
2019-10-28 17:56:28 +0300

    foramtting fix

--- commands.h
+++ commands.h
@@ -71,29 +71,29 @@ if(condition){\
 
 DEF_CMD (PUSH, 	1,  	CMD_ALT (!isalpha(arg[0]) && arg[0]!='[', 	IMMED, 		1, 	StackPush(stack, value))
                   	CMD_ALT (arg[1] == 'x', 			REG, 		11, 	StackPush(stack, registers[arg]))
-                  	CMD_ALT (arg[0]== '[' && arg[2]!='x', 		MEM_IMMED, 		21, 	StackPush(stack, mem[atoi(arg+1)]))
-                  	CMD_ALT (arg[0] == '[' && arg[2] == 'x', 	REG_IMMED, 		31, 	StackPush(stack, mem[registers[arg]])))
+                  	CMD_ALT (arg[0]== '[' && arg[2]!='x', 		MEM_IMMED, 	21, 	StackPush(stack, mem[atoi(arg+1)]))
+                  	CMD_ALT (arg[0] == '[' && arg[2] == 'x', 	REG_IMMED, 	31, 	StackPush(stack, mem[registers[arg]])))
 
 DEF_CMD (POP, 	1, 	CMD_ALT (arg[1] == 'x', 			REG, 		2, 	registers[arg] = StackPop(stack, &status))
-                	CMD_ALT (arg[0] == '[' && arg[2] != 'x', 	MEM_IMMED, 		22, 	mem[atoi(arg+1)] = StackPop(stack, &status))
-                	CMD_ALT (arg[0] == '[' && arg[2] == 'x', 	MEM_REG, 		32, 	mem[registers[arg]] = StackPop(stack, &status)))
+                	CMD_ALT (arg[0] == '[' && arg[2] != 'x', 	MEM_IMMED, 	22, 	mem[atoi(arg+1)] = StackPop(stack, &status))
+                	CMD_ALT (arg[0] == '[' && arg[2] == 'x', 	MEM_REG, 	32, 	mem[registers[arg]] = StackPop(stack, &status)))
 
-DEF_CMD (ADD, 	0, 	CMD_ALT (true, 					NO_ARG, 		3, 	StackPush(stack, StackPop(stack, &status) + StackPop(stack, &status))))
+DEF_CMD (ADD, 	0, 	CMD_ALT (true, 					NO_ARG, 	3, 	StackPush(stack, StackPop(stack, &status) + StackPop(stack, &status))))
 
-DEF_CMD (SUB, 	0, 	CMD_ALT (true, 					NO_ARG, 		4, 	StackPush(stack, StackPop(stack, &status) - StackPop(stack, &status))))
+DEF_CMD (SUB, 	0, 	CMD_ALT (true, 					NO_ARG, 	4, 	StackPush(stack, StackPop(stack, &status) - StackPop(stack, &status))))
 
-DEF_CMD (MUL, 	0, 	CMD_ALT (true, 					NO_ARG, 		5, 	StackPush(stack, StackPop(stack, &status) * StackPop(stack, &status))))
+DEF_CMD (MUL, 	0, 	CMD_ALT (true, 					NO_ARG, 	5, 	StackPush(stack, StackPop(stack, &status) * StackPop(stack, &status))))
 
-DEF_CMD (DIV,  	0, 	CMD_ALT (true, 					NO_ARG, 		6, 	StackPush(stack, StackPop(stack, &status) / StackPop(stack, &status))))
+DEF_CMD (DIV,  	0, 	CMD_ALT (true, 					NO_ARG, 	6, 	StackPush(stack, StackPop(stack, &status) / StackPop(stack, &status))))
 
-DEF_CMD (OUT, 	0, 	CMD_ALT (true, 					NO_ARG, 		15, 	printf("%d\n", StackPop(stack, &status))))
+DEF_CMD (OUT, 	0, 	CMD_ALT (true, 					NO_ARG, 	15, 	printf("%d\n", StackPop(stack, &status))))
 
-DEF_CMD (IN, 	0, 	CMD_ALT (true, 					NO_ARG, 		16, 	scanf("%d", value)))
+DEF_CMD (IN, 	0, 	CMD_ALT (true, 					NO_ARG, 	16, 	scanf("%d", value)))
 
 DEF_CMD	(GSET, 	1, 	CMD_ALT (!isalpha(arg[0]), 			IMMED, 		19, 	graphics[arg] = StackPop(stack, &status))
                  	CMD_ALT (arg[1] == 'x', 			REG, 		23, 	graphics[registers[arg]] = StackPop(stack, &status)))
 
-DEF_CMD (GOUT, 	0, 	CMD_ALT (true, 					NO_ARG, 		20,	CLEAR_CONSOLE;
+DEF_CMD (GOUT, 	0, 	CMD_ALT (true, 					NO_ARG, 	20,	CLEAR_CONSOLE;
 												    	for(int i = 0; i < GRAPHICS_BUFFER_LENGTH`; i++){
 														if(i % GRAPHICS_LINE_LENGTH == 0) printf("\n");
 														SET_COLOR(graphics[i]);
